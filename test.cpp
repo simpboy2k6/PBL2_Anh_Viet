fb9415c729f4824847af67e60c6309e3924ee714
simpboy2k6 <minhkhoa1611dn@gmail.com>
2025-10-31 20:23:58 +0700

    use template and create history

--- test.cpp
+++ test.cpp
@@ -1,22 +1,8 @@
 #include<bits/stdc++.h>
+#include"ctdl.h"
 using namespace std;
 
 int main(){
-    int *arr = new int[4];
-    int *tmp = arr;
-    for(int i=0;i<4;i++){
-        cin>>*(arr+i);
-    }
-    for(int i=0;i<4;i++){
-        cout<<*(arr+i)<<" ";
-
-    }
-    cout<<endl;
-    delete[] arr;
-    arr = new int[5];
-    arr = tmp;
-    *(arr + 4) = 10;
-    for(int i=0;i<5;i++){
-        cout<<*(arr+i)<<" ";
-    }
-}
+    Point<co> p(3,4);
+    p.show();
+}   
